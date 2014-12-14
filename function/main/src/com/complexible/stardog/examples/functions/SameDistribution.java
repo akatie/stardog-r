3177d867bb1a6be93699dde486c01907a8ece445
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2014-12-14 12:20:18 +0100

    connecting using snarl api

--- function/main/src/com/complexible/stardog/examples/functions/SameDistribution.java
+++ function/main/src/com/complexible/stardog/examples/functions/SameDistribution.java
@@ -18,9 +18,19 @@ package com.complexible.stardog.examples.functions;
 import com.complexible.common.base.Strings2;
 import com.complexible.common.rdf.model.Namespaces;
 import com.complexible.common.rdf.model.StardogValueFactory;
+import com.complexible.common.rdf.query.resultio.TextTableQueryResultWriter;
+import com.complexible.stardog.api.Connection;
+import com.complexible.stardog.api.ConnectionConfiguration;
+import com.complexible.stardog.api.SelectQuery;
 import com.complexible.stardog.plan.filter.functions.AbstractFunction;
 import com.complexible.stardog.plan.filter.functions.FunctionEvaluationException;
+import com.complexible.stardog.StardogException;
+
 import org.openrdf.model.Value;
+import org.openrdf.query.resultio.QueryResultIO;
+import org.openrdf.query.TupleQueryResult;
+import org.openrdf.query.TupleQueryResultHandlerException;
+import org.openrdf.query.QueryEvaluationException;
 
 import static com.complexible.common.rdf.model.Values.literal;
 
@@ -31,6 +41,7 @@ import org.rosuda.JRI.RVector;
 import org.rosuda.JRI.RMainLoopCallbacks;
 
 import java.util.Vector;
+import java.io.IOException;
 
 /**
  * <p>SameDistribution SPARQL custom function - runs wilcox.test in R</p>
@@ -41,6 +52,9 @@ import java.util.Vector;
  *
  */
 public final class SameDistribution extends AbstractFunction {
+    // accumulation of bindings
+    private Vector<Value> accBindings = new Vector<Value>();
+
     // this function from a SPARQL query: `bind(stardog:titleCase(?var) as ?tc)`
     protected SameDistribution() {
 	super(1 /* takes a single argument */, Namespaces.STARDOG+"sameDistribution");
@@ -48,6 +62,37 @@ public final class SameDistribution extends AbstractFunction {
 
     @Override
     protected Value internalEvaluate(final Value... theArgs) throws FunctionEvaluationException {
+	// Using SNARL API to open connection to the db to run our own SPARQL queries
+	Connection aConn = null;
+	try {
+	    aConn = ConnectionConfiguration
+		.to("people")
+		.credentials("admin", "admin")
+		.connect();
+
+	    SelectQuery aQuery = aConn.select("select * where { ?s ?p ?o }");
+	    aQuery.limit(10);
+	    TupleQueryResult aResult = aQuery.execute();
+
+	    try {
+		System.out.println("The first ten results...");
+		QueryResultIO.write(aResult, TextTableQueryResultWriter.FORMAT, System.out);
+		aResult.close();
+	    } catch (IOException e) {
+		System.err.println(e.getMessage());
+	    } catch (TupleQueryResultHandlerException e) {
+		System.err.println(e.getMessage());
+	    } catch (QueryEvaluationException e) {
+		System.err.println(e.getMessage());
+	    }
+
+	    aConn.close();
+	} catch (StardogException e) {
+	    System.err.println(e.getMessage());
+	}
+	
+	// If parameter is not a slice, return error
+	
 	// Start JRI R session
 	Rengine re = Rengine.getMainEngine();
 	if(re == null)
@@ -57,7 +102,12 @@ public final class SameDistribution extends AbstractFunction {
 	REXP result = re.eval("(sum(x))");
 	re.end();
 
-	System.out.println(theArgs[0]);
+	accBindings.add(theArgs[0]);
+
+	System.out.println("== Starting accBindings dump ==");
+	for (int i=0; i < accBindings.size(); i++)
+	    System.out.println(accBindings.get(i));
+	System.out.println("== Finishing accBindings dump ==");
 	
 	return literal(result.asDouble());
     }
